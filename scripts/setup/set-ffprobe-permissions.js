3e671efc72fb30e162e52fede90379244d8bf81b
Louistiti <louis.grenard@gmail.com>
2026-03-26 23:03:49 +0800

    fix(scripts): remove chmod noise for Windows on setup

--- scripts/setup/set-ffprobe-permissions.js
+++ scripts/setup/set-ffprobe-permissions.js
@@ -2,12 +2,20 @@ import fs from 'node:fs'
 
 import { path as ffprobePath } from '@ffprobe-installer/ffprobe'
 
+import { SystemHelper } from '@/helpers/system-helper'
+
 import { createSetupStatus } from './setup-status'
 
 export default async () => {
   const status = createSetupStatus('Checking ffprobe permissions...').start()
 
   try {
+    if (SystemHelper.isWindows()) {
+      status.succeed('ffprobe: ready')
+
+      return
+    }
+
     await fs.promises.chmod(ffprobePath, 0o755)
 
     status.succeed('ffprobe: ready')
